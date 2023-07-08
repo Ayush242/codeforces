4b067adefbc0143ad8666410fa09c7eb9443a196
Ayush Mittal <45912958+Ayush242@users.noreply.github.com>
2023-07-08 20:10:09 +0530

    Update Problem A - Where are my Chocolates?.cpp

--- PS 1: Basic IO, Arithmetic/Problem A - Where are my Chocolates?.cpp
+++ PS 1: Basic IO, Arithmetic/Problem A - Where are my Chocolates?.cpp
@@ -1,39 +1,40 @@
-# A. Where are my Chocolates?
-                time limit per test:1 second
-                memory limit per test:256 megabytes
-                input:standard input
-                output:standard output
-Chahel, Shaun, Haricharan and Abhinav live in a hostel, and they have a, b, c, d chocolates respectievely. One night, Dev secretly sneaks inside their rooms and steals all the chocolates.
-Can you help him out try to figure out how many chocolates he has?
+//   A. Where are my Chocolates?
+//                 time limit per test:1 second
+//                 memory limit per test:256 megabytes
+//                 input:standard input
+//                 output:standard output
+// Chahel, Shaun, Haricharan and Abhinav live in a hostel, and they have a, b, c, d chocolates respectievely. One night, Dev secretly sneaks inside their rooms and steals all the chocolates.
+// Can you help him out try to figure out how many chocolates he has?
 
-### Input
-The first line contains 4 integers: a,b,c,d (1≤a,b,c,d≤100) — the number of chocolates they have each.
+//     Input
+// The first line contains 4 integers: a,b,c,d (1≤a,b,c,d≤100) — the number of chocolates they have each.
 
-### Output
-Output a single integer — the number of chocolates Dev has after the theft.
-```
-Examples
-input 
-2 4 5 7
-output 
-18
-input 
-1 9 11 59
-output 
-80
-```
-### Note
-In the first example, they have 2,4,5,7
- chocolates each. So Dev steals 2+4+5+7=18
- chocolates in total.
+//     Output
+// Output a single integer — the number of chocolates Dev has after the theft.
+// ```
+// Examples
+// input 
+// 2 4 5 7
+// output 
+// 18
+// input 
+// 1 9 11 59
+// output 
+// 80
+// ```
+//     Note
+// In the first example, they have 2,4,5,7
+//  chocolates each. So Dev steals 2+4+5+7=18
+//  chocolates in total.
 
-In the second example, they have 1,9,11,59
- chocolates each. So Dev steals 1+9+11+59=80
- chocolates in total.
-# SOLUTION
-```c++
-#include<iostream>
-#include<numeric>
+// In the second example, they have 1,9,11,59
+//  chocolates each. So Dev steals 1+9+11+59=80
+//  chocolates in total.
+
+
+//   SOLUTION
+ include<iostream>
+ include<numeric>
 using namespace std;
  
 int main(){
@@ -45,4 +46,4 @@ int main(){
     cout<< accumulate(a,a+4,sum);
    
 }
-```
+
