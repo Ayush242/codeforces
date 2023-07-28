df454163ace1d301b8dcbb75fe2942bb9c357506
Ayush Mittal <45912958+Ayush242@users.noreply.github.com>
2023-07-29 01:42:22 +0530

    Update Problem A - Again Twenty Five!.cpp

--- PS2 If-Else and Loops/Problem A - Again Twenty Five!.cpp
+++ PS2 If-Else and Loops/Problem A - Again Twenty Five!.cpp
@@ -1,1 +1,29 @@
+// A. Again Twenty Five!
+// time limit per test0.5 seconds
+// memory limit per test64 megabytes
+// inputstandard input
+// outputstandard output
+// The HR manager was disappointed again. The last applicant failed the interview the same way as 24 previous ones. "Do I give such a hard task?" — the HR manager thought. "Just raise number 5 to the power of n and get last two digits of the number. Yes, of course, n can be rather big, and one cannot find the power using a calculator, but we need people who are able to think, not just follow the instructions."
 
+// Could you pass the interview in the machine vision company in IT City?
+
+// Input
+// The only line of the input contains a single integer n (2 ≤ n ≤ 2·1018) — the power in which you need to raise number 5.
+
+// Output
+// Output the last two digits of 5n without spaces between them.
+// Examples
+// input 
+// 2
+// output 
+// 25
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+ 
+int main(){https://codeforces.com/group/7mxf4oQFY7/contest/452362/customtest
+    ll n;
+    cin>>n;
+    cout<<25;
+}
